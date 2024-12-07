3bc1b2deb888490ae5b1e1dc61714423a2153a42
maidesu <vorpeti@gmail.com>
2024-12-07 22:43:34 +0100

    fix warnings

--- src/main/java/main/SheetMusicMaker.java
+++ src/main/java/main/SheetMusicMaker.java
@@ -2,20 +2,7 @@ package main;
 import gui.components.MainFrame;
 
 public class SheetMusicMaker {
-    public static void main(String[] args) {
-//        JFrame frame = new JFrame("Sheet Music Maker");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(800, 600);
-//
-//        frame.setLayout(new BorderLayout());
-//
-//        SheetMusicCanvas canvas = new SheetMusicCanvas();
-//        frame.add(canvas, BorderLayout.CENTER);
-//
-//        JPanel menuBar = new MenuBarPanel(canvas, frame);
-//        frame.add(menuBar, BorderLayout.WEST);
-//
-//        frame.setVisible(true);
-    	MainFrame frame = new MainFrame();
-    }
+	public static void main(String[] args) {
+		new MainFrame();
+	}
 }
